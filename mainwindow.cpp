3c29337dd8a3e8467dbc5f1a95052741567556f3
covobo <noreply@noreply.mail>
2014-04-02 02:28:17 +0400

    firstable programm, QtStateMachine

---
+++ mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    stateOfGame(new QStateMachine),
+    start(new QState),
+    game(new QState),
+    setting(new QState),
+    youAreWinner(new QState)
+{
+    ui->setupUi(this);
+    initAllStateAndStartMachine();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::initAllStateAndStartMachine(){
+    start->assignProperty(ui->stackedWidget, "currentIndex", START_SCREEN_INDEX);
+    start->addTransition(ui->newGameBtn, SIGNAL(clicked()), game);
+    start->addTransition(ui->settingBtn, SIGNAL(clicked()), setting);
+
+    game->assignProperty(ui->stackedWidget, "currentIndex", GAME_SCREEN_INDEX);
+    game->addTransition(ui->exitGameBtn, SIGNAL(clicked()), start);
+
+    //setting->assignProperty(ui->stackedWidget, "currendIndex", SETTING_SCREEN_INDEX);
+
+    stateOfGame->addState(start);
+    stateOfGame->addState(game);
+    //stateOfGame->addState(setting);
+    //stateOfGame->addState(youAreWinner);
+    stateOfGame->setInitialState(start);
+    stateOfGame->start();
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,43 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#define GAME_SCREEN_INDEX 1
+#define START_SCREEN_INDEX 0
+#define SETTING_SCREEN_INDEX 2
+
+#include <QMainWindow>
+#include <QStateMachine>
+#include <QState>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void makeTransition();
+
+private slots:
+    void on_newGameBtn_clicked();
+
+    void on_exitGameBtn_clicked();
+
+signals:
+    void openWindow(QString key);
+
+private:
+    Ui::MainWindow *ui;
+    QStateMachine *stateOfGame;
+    QState *start;
+    QState *game;
+    QState *setting;
+    QState *youAreWinner;
+    void initAllStateAndStartMachine();
+};
+
+#endif // MAINWINDOW_H
